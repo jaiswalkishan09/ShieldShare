c53b86fcd8a44676ea4f412b57206444adb86782
kishan <jaiswalkishan0010@gmail.com>
2024-10-02 13:48:18 +0545

    added error handling for database connection

--- backend/src/controllers/authRouteControllers.js
+++ backend/src/controllers/authRouteControllers.js
@@ -16,29 +16,35 @@ const {
 const { tables } = require("../common/tableAlias");
 
 const getUserDetails = async (req, res) => {
-  let connectDb = await dbConnection.getDataBaseConnection();
-  const databaseConnection = knex(connectDb.connection);
   try {
-    let userId = req.userId;
-    let userData = await getUserDetailsBasedOnUserId(
-      databaseConnection,
-      userId
-    );
+    let connectDb = await dbConnection.getDataBaseConnection();
+    const databaseConnection = knex(connectDb.connection);
+    try {
+      let userId = req.userId;
+      let userData = await getUserDetailsBasedOnUserId(
+        databaseConnection,
+        userId
+      );
 
-    if (userData.length > 0) {
-      userData = userData[0];
+      if (userData.length > 0) {
+        userData = userData[0];
+      }
+      databaseConnection ? databaseConnection.destroy() : null;
+      return res.status(200).json({
+        userName: userData.User_Name,
+        firstName: userData.First_Name,
+        lastName: userData.Last_Name,
+        email: userData.Email,
+        mobileNo: userData.Mobile_No,
+      });
+    } catch (e) {
+      databaseConnection ? databaseConnection.destroy() : null;
+      console.log("Error in getUserDetails main catch block", e);
+      return res
+        .status(500)
+        .json({ message: "Something went wrong please try again." });
     }
-    databaseConnection ? databaseConnection.destroy() : null;
-    return res.status(200).json({
-      userName: userData.User_Name,
-      firstName: userData.First_Name,
-      lastName: userData.Last_Name,
-      email: userData.Email,
-      mobileNo: userData.Mobile_No,
-    });
   } catch (e) {
-    databaseConnection ? databaseConnection.destroy() : null;
-    console.log("Error in getUserDetails main catch block", e);
     return res
       .status(500)
       .json({ message: "Something went wrong please try again." });
@@ -75,23 +81,29 @@ const getAllUserDetails = async (req, res) => {
 };
 
 const addRequest = async (req, res) => {
-  const { requestedTo } = req.body;
-  let userId = req.userId;
-  let connectDb = await dbConnection.getDataBaseConnection();
-  const databaseConnection = knex(connectDb.connection);
-
   try {
-    const data = {
-      Requested_By: userId,
-      Requested_To: requestedTo,
-      Requested_Date: DateTime.utc().toISO(),
-      Request_Status: "PENDING",
-    };
-    await insertIntoTable(databaseConnection, data, tables.userRequest);
-    return res.status(201).json({ requestedTo });
+    const { requestedTo } = req.body;
+    let userId = req.userId;
+    let connectDb = await dbConnection.getDataBaseConnection();
+    const databaseConnection = knex(connectDb.connection);
+
+    try {
+      const data = {
+        Requested_By: userId,
+        Requested_To: requestedTo,
+        Requested_Date: DateTime.utc().toISO(),
+        Request_Status: "PENDING",
+      };
+      await insertIntoTable(databaseConnection, data, tables.userRequest);
+      return res.status(201).json({ requestedTo });
+    } catch (e) {
+      databaseConnection ? databaseConnection.destroy() : null;
+      console.log("Error in addRequest main catch block", e);
+      return res
+        .status(500)
+        .json({ message: "Something went wrong please try again." });
+    }
   } catch (e) {
-    databaseConnection ? databaseConnection.destroy() : null;
-    console.log("Error in addRequest main catch block", e);
     return res
       .status(500)
       .json({ message: "Something went wrong please try again." });
@@ -100,19 +112,25 @@ const addRequest = async (req, res) => {
 
 const getAllUserHistory = async (req, res) => {
   try {
-    let connectDb = await dbConnection.getDataBaseConnection();
-    const databaseConnection = knex(connectDb.connection);
     try {
-      let userId = req.userId;
-      let userData = await getUserHistory(databaseConnection, userId);
+      let connectDb = await dbConnection.getDataBaseConnection();
+      const databaseConnection = knex(connectDb.connection);
+      try {
+        let userId = req.userId;
+        let userData = await getUserHistory(databaseConnection, userId);
 
-      databaseConnection ? databaseConnection.destroy() : null;
-      return res.status(200).json({
-        requestData: userData,
-      });
+        databaseConnection ? databaseConnection.destroy() : null;
+        return res.status(200).json({
+          requestData: userData,
+        });
+      } catch (e) {
+        databaseConnection ? databaseConnection.destroy() : null;
+        console.log("Error in getAllUserHistory main catch block", e);
+        return res
+          .status(500)
+          .json({ message: "Something went wrong please try again." });
+      }
     } catch (e) {
-      databaseConnection ? databaseConnection.destroy() : null;
-      console.log("Error in getAllUserHistory main catch block", e);
       return res
         .status(500)
         .json({ message: "Something went wrong please try again." });
@@ -180,41 +198,48 @@ const getApprovalHistoryRequest = async (req, res) => {
 };
 
 const updateRequest = async (req, res) => {
-  const { requestId, status, firstName, lastName, email, mobileNo } = req.body;
-
-  let connectDb = await dbConnection.getDataBaseConnection();
-  const databaseConnection = knex(connectDb.connection);
-
   try {
-    let data = "";
-    if (status === "REJECTED") {
-      data = {
-        Request_Updated_On: DateTime.utc().toISO(),
-        Request_Status: "REJECTED",
-      };
-    } else if (status === "APPROVED") {
-      data = {
-        Request_Updated_On: DateTime.utc().toISO(),
-        Request_Status: "APPROVED",
-        First_Name: firstName,
-        Last_Name: lastName,
-        Email: email,
-        Mobile_No: mobileNo,
-      };
-    } else {
-      return res.status(400).json({ message: "Invalid input" });
-    }
+    const { requestId, status, firstName, lastName, email, mobileNo } =
+      req.body;
 
-    await updateRequestTable(
-      databaseConnection,
-      data,
-      tables.userRequest,
-      requestId
-    );
-    return res.status(201).json({ message: "Request updated successfully." });
+    let connectDb = await dbConnection.getDataBaseConnection();
+    const databaseConnection = knex(connectDb.connection);
+
+    try {
+      let data = "";
+      if (status === "REJECTED") {
+        data = {
+          Request_Updated_On: DateTime.utc().toISO(),
+          Request_Status: "REJECTED",
+        };
+      } else if (status === "APPROVED") {
+        data = {
+          Request_Updated_On: DateTime.utc().toISO(),
+          Request_Status: "APPROVED",
+          First_Name: firstName,
+          Last_Name: lastName,
+          Email: email,
+          Mobile_No: mobileNo,
+        };
+      } else {
+        return res.status(400).json({ message: "Invalid input" });
+      }
+
+      await updateRequestTable(
+        databaseConnection,
+        data,
+        tables.userRequest,
+        requestId
+      );
+      return res.status(201).json({ message: "Request updated successfully." });
+    } catch (e) {
+      databaseConnection ? databaseConnection.destroy() : null;
+      console.log("Error in updateRequest main catch block", e);
+      return res
+        .status(500)
+        .json({ message: "Something went wrong please try again." });
+    }
   } catch (e) {
-    databaseConnection ? databaseConnection.destroy() : null;
-    console.log("Error in updateRequest main catch block", e);
     return res
       .status(500)
       .json({ message: "Something went wrong please try again." });
--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -14,52 +14,59 @@ const dbConnection = require("../common/connection");
 const { DateTime } = require("luxon");
 
 const signUp = async (req, res) => {
-  let SECRET_KEY = process.env.SECRET_KEY;
-  const { uName, firstName, lastName, email, mobileNo, password, publicKey } =
-    req.body;
-  let connectDb = await dbConnection.getDataBaseConnection();
-  const databaseConnection = knex(connectDb.connection);
   try {
-    let userExist = await checkExistingUser(databaseConnection, uName);
+    let SECRET_KEY = process.env.SECRET_KEY;
+    const { uName, firstName, lastName, email, mobileNo, password, publicKey } =
+      req.body;
+    let connectDb = await dbConnection.getDataBaseConnection();
+    const databaseConnection = knex(connectDb.connection);
+    try {
+      let userExist = await checkExistingUser(databaseConnection, uName);
 
-    if (userExist == "User don't exist") {
-      const hashPassword = await bcrypt.hash(password, 10);
+      if (userExist == "User don't exist") {
+        const hashPassword = await bcrypt.hash(password, 10);
 
-      let data = {
-        User_Name: uName,
-        First_Name: firstName,
-        Last_Name: lastName,
-        Email: email,
-        Mobile_No: mobileNo,
-        Password: hashPassword,
-        Public_Key: publicKey,
-        Created_On: DateTime.utc().toISO(),
-      };
-      let userId = await insertIntoTable(
-        databaseConnection,
-        data,
-        tables.userBasicDetails
-      );
-      let token = jwt.sign({ userId: userId[0], uName: uName }, SECRET_KEY);
+        let data = {
+          User_Name: uName,
+          First_Name: firstName,
+          Last_Name: lastName,
+          Email: email,
+          Mobile_No: mobileNo,
+          Password: hashPassword,
+          Public_Key: publicKey,
+          Created_On: DateTime.utc().toISO(),
+        };
+        let userId = await insertIntoTable(
+          databaseConnection,
+          data,
+          tables.userBasicDetails
+        );
+        let token = jwt.sign({ userId: userId[0], uName: uName }, SECRET_KEY);
+        databaseConnection ? databaseConnection.destroy() : null;
+        return res.status(201).json({
+          uName: uName,
+          token: token,
+        });
+      } else if (userExist == "User exist") {
+        databaseConnection ? databaseConnection.destroy() : null;
+        return res.status(400).json({
+          message:
+            "The username already exists. Please choose a different one.",
+        });
+      } else {
+        databaseConnection ? databaseConnection.destroy() : null;
+        return res
+          .status(500)
+          .json({ message: "Something went wrong please try again." });
+      }
+    } catch (e) {
       databaseConnection ? databaseConnection.destroy() : null;
-      return res.status(201).json({
-        uName: uName,
-        token: token,
-      });
-    } else if (userExist == "User exist") {
-      databaseConnection ? databaseConnection.destroy() : null;
-      return res.status(400).json({
-        message: "The username already exists. Please choose a different one.",
-      });
-    } else {
-      databaseConnection ? databaseConnection.destroy() : null;
+      console.log("Error in signUp main catch block", e);
       return res
         .status(500)
         .json({ message: "Something went wrong please try again." });
     }
   } catch (e) {
-    databaseConnection ? databaseConnection.destroy() : null;
-    console.log("Error in signUp main catch block", e);
     return res
       .status(500)
       .json({ message: "Something went wrong please try again." });
@@ -67,49 +74,55 @@ const signUp = async (req, res) => {
 };
 
 const signIn = async (req, res) => {
-  let SECRET_KEY = process.env.SECRET_KEY;
-  let connectDb = await dbConnection.getDataBaseConnection();
-  const databaseConnection = knex(connectDb.connection);
-  const { uName, password } = req.body;
   try {
-    let userDetails = await getUserDetailsBasedOnUname(
-      databaseConnection,
-      uName
-    );
-    if (userDetails) {
-      if (userDetails.length == 0) {
-        databaseConnection ? databaseConnection.destroy() : null;
-        return res.status(404).json({ message: "User not found" });
-      } else {
-        userDetails = userDetails[0];
-        const matchPassword = await bcrypt.compare(
-          password,
-          userDetails.Password
-        );
-        if (!matchPassword) {
+    let SECRET_KEY = process.env.SECRET_KEY;
+    let connectDb = await dbConnection.getDataBaseConnection();
+    const databaseConnection = knex(connectDb.connection);
+    const { uName, password } = req.body;
+    try {
+      let userDetails = await getUserDetailsBasedOnUname(
+        databaseConnection,
+        uName
+      );
+      if (userDetails) {
+        if (userDetails.length == 0) {
           databaseConnection ? databaseConnection.destroy() : null;
-          return res.status(400).json({ message: "Invalid Credentials" });
-        }
+          return res.status(404).json({ message: "User not found" });
+        } else {
+          userDetails = userDetails[0];
+          const matchPassword = await bcrypt.compare(
+            password,
+            userDetails.Password
+          );
+          if (!matchPassword) {
+            databaseConnection ? databaseConnection.destroy() : null;
+            return res.status(400).json({ message: "Invalid Credentials" });
+          }
 
-        let token = jwt.sign(
-          { userId: userDetails.User_Id, uName: uName },
-          SECRET_KEY
-        );
+          let token = jwt.sign(
+            { userId: userDetails.User_Id, uName: uName },
+            SECRET_KEY
+          );
+          databaseConnection ? databaseConnection.destroy() : null;
+          return res.status(200).json({
+            uName: uName,
+            token: token,
+          });
+        }
+      } else {
         databaseConnection ? databaseConnection.destroy() : null;
-        return res.status(200).json({
-          uName: uName,
-          token: token,
-        });
+        return res
+          .status(500)
+          .json({ message: "Something went wrong please try again." });
       }
-    } else {
+    } catch (e) {
       databaseConnection ? databaseConnection.destroy() : null;
+      console.log("Error in signin main catch block", e);
       return res
         .status(500)
         .json({ message: "Something went wrong please try again." });
     }
   } catch (e) {
-    databaseConnection ? databaseConnection.destroy() : null;
-    console.log("Error in signin main catch block", e);
     return res
       .status(500)
       .json({ message: "Something went wrong please try again." });
